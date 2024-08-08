0725ebb32a15511d71cb0509dd479e659c266ca0
sarathkp <ssarathckm@gmail.com>
2024-08-08 18:32:39 +0530

    The whole file is replaced with a new Carousel.jsx

--- src/components/Product/Carousel.jsx
+++ src/components/Product/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, memo } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 
 import classNames from "classnames";
 import { useShowProduct } from "hooks/reactQuery/useProductsApi";
@@ -13,24 +13,29 @@ const Carousel = () => {
   const timerRef = useRef(null);
   const { data: { imageUrl, imageUrls: partialImageUrls, title } = {} } =
     useShowProduct(slug);
-
   const imageUrls = append(imageUrl, partialImageUrls);
 
-  const handleNext = () => {
+  const handleNext = useCallback(() => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+  }, [imageUrls.length]);
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
   };
 
   const handlePrevious = () => {
     setCurrentIndex(
       (prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length
     );
+    resetTimer();
   };
 
   useEffect(() => {
     timerRef.current = setInterval(handleNext, 3000);
 
     return () => clearInterval(timerRef.current);
-  }, []);
+  }, [handleNext]); // Dependency array includes handleNext
 
   return (
     <div className="flex flex-col items-center">
@@ -43,7 +48,6 @@ const Carousel = () => {
           onClick={handlePrevious}
         />
         <img
-          alt={title}
           className="max-w-56 h-56 max-h-56 w-56"
           src={imageUrls[currentIndex]}
         />
@@ -51,10 +55,13 @@ const Carousel = () => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={handleNext}
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
         />
       </div>
-      <div className="mt-2 flex space-x-1">
+      <div className="flex space-x-1">
         {imageUrls.map((_, index) => (
           <span
             key={index}
@@ -62,7 +69,10 @@ const Carousel = () => {
               "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
               { "neeto-ui-bg-black": index === currentIndex }
             )}
-            onClick={() => setCurrentIndex(index)}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
           />
         ))}
       </div>
@@ -70,4 +80,4 @@ const Carousel = () => {
   );
 };
 
-export default memo(Carousel);
+export default Carousel;
